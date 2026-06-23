f63bdfee4c58d0cb2dfd90ef55bdff159d143ee5
Mads Kvist Møller <mmo@trifork.com>
2026-06-23 15:04:39 +0200

    Add S3 test to test-app

--- test-app/WorkerService1/Program.cs
+++ test-app/WorkerService1/Program.cs
@@ -1,8 +1,16 @@
+using Amazon.S3;
 using WorkerService1;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Logging.AddConsole();
 builder.Services.AddHostedService<Worker>();
+builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
+builder.Services.AddAWSService<IAmazonS3>();
+
+// Amazon.AWSConfigs.LoggingConfig.LogTo = Amazon.LoggingOptions.Console;
+// Amazon.AWSConfigs.LoggingConfig.LogResponses = Amazon.ResponseLoggingOption.Always;
+// Amazon.AWSConfigs.LoggingConfig.LogMetrics = true;
+// Amazon.AWSConfigs.AddTraceListener("Amazon", new System.Diagnostics.ConsoleTraceListener());
 
 var host = builder.Build();
 host.Run();
--- test-app/WorkerService1/Worker.cs
+++ test-app/WorkerService1/Worker.cs
@@ -1,7 +1,11 @@
+using Amazon.S3;
+using Amazon.S3.Model;
+
 namespace WorkerService1;
 
-public class Worker(ILogger<Worker> logger) : BackgroundService
+public class Worker(ILogger<Worker> logger, IAmazonS3 s3, IConfiguration configuration) : BackgroundService
 {
+    private const string S3_KEY = "uploads/test.txt";
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
@@ -11,7 +15,46 @@ public class Worker(ILogger<Worker> logger) : BackgroundService
                 logger.LogInformation("Worker {machineId} running at: {time}", Environment.MachineName, DateTimeOffset.Now);
             }
 
-            await File.AppendAllTextAsync("/data/payload-test.txt", $"Worker {Environment.MachineName} writing at: {DateTimeOffset.Now}\n", stoppingToken);
+            var bucketsResponse = await s3.ListBucketsAsync(stoppingToken);
+            var buckets = bucketsResponse.Buckets.Select(b => b.BucketName);
+            if (logger.IsEnabled(LogLevel.Information))
+            {
+                logger.LogInformation("S3 Buckets available: {buckets}", buckets);
+            }
+            
+            var bucketName = configuration["S3:BucketName"];
+            var readRequest = new GetObjectRequest
+            {
+                BucketName = bucketName,
+                Key = S3_KEY
+            };
+
+            try
+            {
+                using var response = await s3.GetObjectAsync(readRequest, stoppingToken);
+                using var streamReader = new StreamReader(response.ResponseStream);
+                var content = await streamReader.ReadToEndAsync(stoppingToken);
+                if (logger.IsEnabled(LogLevel.Information))
+                {
+                    logger.LogInformation("Content of {s3key} in {bucket} is: {response}", S3_KEY, bucketName, content);
+                }
+            }
+            catch (Exception e)
+            {
+                logger.LogWarning("S3 read request failed: {e}", e);
+            }
+            var request = new PutObjectRequest()
+            {
+                BucketName = bucketName,
+                Key = S3_KEY,
+                ContentBody = $"Hello World! I am: {Environment.MachineName}",
+                ContentType = "text/plain",
+                UseChunkEncoding = false,
+                DisableDefaultChecksumValidation = true
+            };
+            await s3.PutObjectAsync(request, stoppingToken);
+
+            await File.AppendAllTextAsync($"/data/payload-test-{Environment.MachineName}.txt", $"Worker writing at: {DateTimeOffset.Now}\n", stoppingToken);
             
             await Task.Delay(1000, stoppingToken);
         }
